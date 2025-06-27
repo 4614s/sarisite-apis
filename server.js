142877e642e6d951b1d19fbc0cfd201128af9063
4614s <onurgral1446.2@gmail.com>
2025-06-27 16:47:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use((req, res) => {
-  res.status(404).send('<h1>404 - Sayfa Bulunamadı</h1><br><p>Üzgünüz, aradığınız sayfa mevcut değil.</p>');
-});
+res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 
 
 app.listen(port, () => {
