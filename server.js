78b259794f7ebb754c7717c43ffab64ebd8673b0
4614s <onurgral1446.2@gmail.com>
2025-06-27 16:41:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Sayfa Bulunamadı</h1><br><p>Üzgünüz, aradığınız sayfa mevcut değil.</p>');
+});
+
+
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda çalışıyor.`);
 });
